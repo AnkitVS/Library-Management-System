8790d1439998d68611716a8725c6882887ae1e15
Ankit Ved <42760900+AnkitVS@users.noreply.github.com>
2024-09-10 23:54:56 +0530

    Changes

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -6,18 +6,26 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
+	private String password,userid;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		password = request.getParameter("password");
+		userid = request.getParameter("userId");
+		
+		response.setContentType("text/html");
+		PrintWriter write = response.getWriter();
+		
+		write.print("Password:"+password+"\nUuserid:"+userid);
 	}
 
 }
